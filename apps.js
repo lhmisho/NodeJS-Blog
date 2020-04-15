c60c43ebc0f3f4ad9fba789b88744ac0a744b54c
lokman hossen <lokman@divine-it.net>
2020-04-15 22:08:54 +0600

    installing ejs and setup app.js with middleware array

--- apps.js
+++ apps.js
@@ -3,6 +3,19 @@ const morgan = require('morgan')
 
 const app = express()
 
+// setup view engine 
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+// middleware array
+const middleware = [
+    morgan('dev'),
+    express.static('public'),
+    express.urlencoded({extended: true}),
+    express.json()
+]
+app.use(middleware)
+
 app.get('/', (req, res) => {
     res.json({
         massage: 'Welcome to new blog'
