b0d36b7f07d3f5cc1c45163d92c2dfa6ec9e3cda
lokman hossen <lokman@divine-it.net>
2020-04-16 17:06:57 +0600

    hashing password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 let User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 exports.signupGetController = (req, res, next) => {
     res.render('pages/auth/signup.ejs', { title: 'Create a new account' })
@@ -6,13 +7,16 @@ exports.signupGetController = (req, res, next) => {
 
 exports.signupPostController = async (req, res, next) => {
     console.log(req.body)
-    let { username, password, email } = req.body
-    let user = new User({
-        username,
-        email,
-        password
-    })
+
     try {
+        let { username, password, email } = req.body
+        let hashedPassword = await bcrypt.hash(password, 11)
+        let user = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+
         let createdUser = await user.save()
         console.log("User successfully created, ", createdUser)
         res.render('pages/auth/signup.ejs', { title: 'Create a new account' })
