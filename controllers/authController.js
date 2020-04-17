7bd9796b2e4f834ebf30b15bf5f3779bd0971c34
lokman hossen <lokman@divine-it.net>
2020-04-17 21:50:33 +0600

    render form data back

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,17 +4,22 @@ const { validationResult } = require('express-validator')
 const errorFormatter = require('../utilitiy/validationErrorFormatter')
 
 exports.signupGetController = (req, res, next) => {
-    res.render('pages/auth/signup.ejs', { title: 'Create a new account', error: {} })
+    res.render('pages/auth/signup.ejs', { title: 'Create a new account', error: {}, value: {} })
 }
 
 exports.signupPostController = async (req, res, next) => {
+    let { username, password, email } = req.body
     let errors = validationResult(req).formatWith(errorFormatter)
     if (errors) {
         console.log(errors.mapped())
-        return res.render('pages/auth/signup.ejs', { title: 'Create a new account', error: errors.mapped() })
+        return res.render('pages/auth/signup.ejs', {
+            title: 'Create a new account', error: errors.mapped(),
+            value: {
+                username, password, email
+            }
+        })
     }
     try {
-        let { username, password, email } = req.body
         let hashedPassword = await bcrypt.hash(password, 11)
         let user = new User({
             username,
@@ -24,7 +29,7 @@ exports.signupPostController = async (req, res, next) => {
 
         let createdUser = await user.save()
         console.log("User successfully created, ", createdUser)
-        res.render('pages/auth/signup.ejs', { title: 'Create a new account' })
+        res.render('pages/auth/signup.ejs', { title: 'Create a new account', error: {}, value: {} })
     } catch (e) {
         console.log(e)
         next(e)
