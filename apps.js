e0c68f4cda079fd19e0427c7bde5d27de2859f63
Lokman Hossen <lokman@divine-it.net>
2020-05-02 23:34:53 +0600

    <config> setup config file

--- apps.js
+++ apps.js
@@ -21,6 +21,13 @@ var store = new MongoDBStore({
     expires: 1000 * 60 * 60 * 24 * 7 // one week
   });
 
+const config = require('./config/config')
+if(app.get('env').toLowerCase() === 'development'){
+    console.log(config.dev.name)
+}
+if(app.get('env').toLowerCase() === 'production'){
+    console.log(config.prod.name)
+}
 
 // const playgroundRouts = require('./playground/validator')
 const authRoutes = require('./routes/authRoutes')
---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    dev: {
+        name: 'Hossen Misho'
+    },
+    prod: {
+        name: "'Lokman's planet"
+    }
+}
